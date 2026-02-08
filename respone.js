4f1ee84841760e5d4a20232eda29ebae3c6bdca1
Rubia Imran <shahjahan1957begum@gmail.com>
2026-02-08 17:31:48 +0500

    colorchangewhenclick

---
+++ respone.js
@@ -0,0 +1,19 @@
+document.querySelector("#Corn").addEventListener("click", function(){
+ document.body.style.backgroundColor="Cornflowerblue";
+});
+
+document.querySelector("#cyan").addEventListener("click", function(){
+    document.body.style.backgroundColor="darkcyan";
+});
+
+document.querySelector("#crimson").addEventListener("click" ,function(){
+    document.body.style.backgroundColor="crimson";
+});
+
+document.querySelector("#salmon").addEventListener("click", function(){
+    document.body.style.backgroundColor="darksalmon";
+});
+
+document.querySelector("#reset").addEventListener("click", function(){
+    document.body.style.backgroundColor="white";
+});
